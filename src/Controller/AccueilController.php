cb17ac37cfe457ec2f888f79168cac3bf0baf71b
Alpha DIOP <alphakhoss@gmail.com>
2023-03-09 11:52:37 +0100

    MyBlog

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
+    private $mr;
+    public function __construct(ManagerRegistry $mr)
+    {
+        $this->mr = $mr;
+    }
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
+        $articles = $this->mr->getRepository(Article::class)->findAll();
         return $this->render('accueil/index.html.twig', [
             'title' => 'Accueil',
+            'articles' => $articles,
         ]);
     }
 }
--- src/Controller/AjoutarticleController.php
+++ src/Controller/AjoutarticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,13 +12,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AjoutarticleController extends AbstractController
 {
+    public function __construct (ManagerRegistry $mr)
+    {
+        $this->mr = $mr;
+    }
     private $mr;
     #[Route('/ajoutarticle', name: 'app_ajoutarticle')]
     public function index(Request $request): Response
     {
         $article = new Article();
 
+        $entityManager = $this->mr->getManager();
+
         $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager->persist($article);
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_accueil');
+        }
 
         return $this->render('ajoutarticle/index.html.twig', [
             'title' => 'Ajout Article',
